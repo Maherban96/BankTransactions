9929567ca6a06daa02d3567c28fc723429bc1041
maherbanali <ali.sunasara@epistic.net>
2023-02-05 17:27:37 +0530

    Initial commit

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankTransactions.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> option) : base(option)
+        {
+
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
