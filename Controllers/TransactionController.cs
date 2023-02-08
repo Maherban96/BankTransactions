e2cd6499d49367bc143882174a69b595a2b0bbcf
Maherbanali <maherban.ali@spacestem.com>
2023-02-08 14:50:18 +0530

    commit the bank transaction file in asp .met mvc

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,95 @@
+﻿using BankTransactions.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankTransactions.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public TransactionDbContext _context { get; }
+        public TransactionController(TransactionDbContext context)
+        {
+            this._context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var transaction = await _context.Transactions.ToListAsync();
+            return View(transaction);
+        }
+
+        public IActionResult Create()
+        {   
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Transaction transactions)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transactions);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transactions);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || id <= 0)
+                return BadRequest();
+
+            var transactionDB = await _context.Transactions.FirstOrDefaultAsync(t => t.TransactionId == id);
+            if(transactionDB is null)
+            {
+                return NotFound();
+            }
+            return View(transactionDB);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Transaction transaction)
+        {
+            if (!ModelState.IsValid)
+                return View(transaction);
+            
+            _context.Transactions.Update(transaction);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || id <= 0)
+                return BadRequest();
+
+            var transactionDB = await _context.Transactions.FirstOrDefaultAsync(t => t.TransactionId == id);
+            if (transactionDB is null)
+            {
+                return NotFound();
+            }
+            _context.Transactions.Remove(transactionDB);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null || id <= 0)
+                return BadRequest();
+
+            var transactionDB = await _context.Transactions.FirstOrDefaultAsync(t => t.TransactionId == id);
+            if (transactionDB is null)
+            {
+                return NotFound();
+            }
+            return View(transactionDB);
+        }
+    }
+}
---
+++ Migrations/20230208080232_AddedDataAnotationsToTransactionModel.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankTransactions.Migrations
+{
+    public partial class AddedDataAnotationsToTransactionModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SWIFTCode",
+                table: "Transactions",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeneficiaryName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "Transactions",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SWIFTCode",
+                table: "Transactions",
+                type: "nvarchar(11)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeneficiaryName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "Transactions",
+                type: "nvarchar(12)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+    }
+}
